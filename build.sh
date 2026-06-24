52bfe1278b4bdfd2e6a3cb7d4a759cde832bd3f7
Solayem U. Ansari <ansari.solayem@apmc.doh.local>
2026-06-24 13:46:56 +0800

    database connection 2

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
+set -o errexit
+
+# 1. Install all dependencies from your file
+pip install -r requirements.txt
+
+# 2. Collect static files (optional but highly recommended for Django)
+python manage.py collectstatic --noinput
+
+# 3. Swap the string out to Session mode and run your migrations
+DATABASE_URL=$DIRECT_URL python manage.py migrate --noinput
