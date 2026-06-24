8ebd8cd9785ae1e61f0603a3e7d0630f299ca838
Solayem U. Ansari <ansari.solayem@apmc.doh.local>
2026-06-24 08:53:02 +0800

    home commit

--- emalexpots/views.py
+++ emalexpots/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 def home(request):
     """Serve the React build (index.html) from emalexpots/static/static/."""
 
-    index_path = Path(__file__).resolve().parent / "static" / "static" / "index.html"
+    index_path = Path(__file__).resolve().parent / "static" / "index.html"
     return HttpResponse(index_path.read_text(encoding="utf-8"), content_type="text/html")
+
 
