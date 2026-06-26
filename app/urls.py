2d46b02de9685a743ccc3e386ce36876dcb1b99b
Solayem U. Ansari <ansari.solayem@apmc.doh.local>
2026-06-26 14:46:47 +0800

    version 0.0.1c

--- app/urls.py
+++ app/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from dashboard import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home_explicit'),
     path('admin/', admin.site.urls),
+    path(
+        'manifest.json', 
+        TemplateView.as_view(template_name='manifest.json', content_type='application/json'), 
+        name='manifest.json'
+    ),
 ]
